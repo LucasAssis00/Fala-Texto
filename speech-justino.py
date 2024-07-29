9126ce2e431c8e1fb6a421ed0b12260b2633bed5
LucasAssis00 <92377740+LucasAssis00@users.noreply.github.com>
2024-07-29 14:13:21 -0300

    Add files via upload

--- speech-justino.py
+++ speech-justino.py
@@ -12,11 +12,157 @@ import platform
 from gtts import gTTS
 
 
+br_number_system = {
+    'zero': 0,
+    'um': 1,
+    'uma': 1,
+    'dois': 2,
+    'duas': 2,
+    'tres': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10,
+    'onze': 11,
+    'doze': 12,
+    'treze': 13,
+    'catorze': 14,
+    'quinze': 15,
+    'dezesseis': 16,
+    'dezessete': 17,
+    'dezoito': 18,
+    'dezenove': 19,
+    'vinte': 20,
+    'trinta': 30,
+    'quarenta': 40,
+    'cinquenta': 50,
+    'sessenta': 60,
+    'setenta': 70,
+    'oitenta': 80,
+    'noventa': 90,
+    'cem': 100,
+    'cento': 100,
+    #'mil': 1000,
+    #'milhão': 1000000,
+    #'bilhão': 1000000000,
+    #'ponto': '.'
+
+}
+
+def number_formation(number_words):
+    numbers = []
+    for number_word in number_words:
+        numbers.append(br_number_system[number_word])
+#    if len(numbers) == 4:
+#        return (numbers[0] * numbers[1]) + numbers[2] + numbers[3]
+    if len(numbers) == 3:
+        return numbers[0] + numbers[1] + numbers[2]
+    elif len(numbers) == 2:
+        return numbers[0] + numbers[1]
+    else:
+        return numbers[0]
+
+
+def word_to_num(number_sentence):
+    if type(number_sentence) is not str:
+        raise ValueError("Type of input is not string! Please enter a valid number word (eg. \'two million twenty three thousand and forty nine\')")
+
+    number_sentence = number_sentence.replace('-', ' ')
+    number_sentence = number_sentence.lower()  # converting input to lowercase
+
+    if(number_sentence.isdigit()):  # return the number if user enters a number string
+        return int(number_sentence)
+
+    split_words = number_sentence.strip().split()  # strip extra spaces and split sentence into words
+
+    clean_numbers = []
+    clean_decimal_numbers = []
+
+    # removing and, & etc.
+    for word in split_words:
+        if word in br_number_system:
+            clean_numbers.append(word)
+
+    # Error message if the user enters invalid input!
+    if len(clean_numbers) == 0:
+        raise ValueError("No valid number words found! Please enter a valid number word (eg. two million twenty three thousand and forty nine)")
+
+    # Error if user enters million,billion, thousand or decimal point twice
+    if clean_numbers.count('thousand') > 1 or clean_numbers.count('million') > 1 or clean_numbers.count('billion') > 1 or clean_numbers.count('point')> 1:
+        raise ValueError("Redundant number word! Please enter a valid number word (eg. two million twenty three thousand and forty nine)")
+
+    # separate decimal part of number (if exists)
+    if clean_numbers.count('point') == 1:
+        clean_decimal_numbers = clean_numbers[clean_numbers.index('point')+1:]
+        clean_numbers = clean_numbers[:clean_numbers.index('point')]
+
+    billion_index = clean_numbers.index('billion') if 'billion' in clean_numbers else -1
+    million_index = clean_numbers.index('million') if 'million' in clean_numbers else -1
+    thousand_index = clean_numbers.index('thousand') if 'thousand' in clean_numbers else -1
+
+    if (thousand_index > -1 and (thousand_index < million_index or thousand_index < billion_index)) or (million_index>-1 and million_index < billion_index):
+        raise ValueError("Malformed number! Please enter a valid number word (eg. two million twenty three thousand and forty nine)")
+
+    total_sum = 0  # storing the number to be returned
+
+    if len(clean_numbers) > 0:
+        # hack for now, better way TODO
+        if len(clean_numbers) == 1:
+                total_sum += br_number_system[clean_numbers[0]]
+
+        else:
+            if billion_index > -1:
+                billion_multiplier = number_formation(clean_numbers[0:billion_index])
+                total_sum += billion_multiplier * 1000000000
+
+            if million_index > -1:
+                if billion_index > -1:
+                    million_multiplier = number_formation(clean_numbers[billion_index+1:million_index])
+                else:
+                    million_multiplier = number_formation(clean_numbers[0:million_index])
+                total_sum += million_multiplier * 1000000
+
+            if thousand_index > -1:
+                if million_index > -1:
+                    thousand_multiplier = number_formation(clean_numbers[million_index+1:thousand_index])
+                elif billion_index > -1 and million_index == -1:
+                    thousand_multiplier = number_formation(clean_numbers[billion_index+1:thousand_index])
+                else:
+                    thousand_multiplier = number_formation(clean_numbers[0:thousand_index])
+                total_sum += thousand_multiplier * 1000
+
+            if thousand_index > -1 and thousand_index != len(clean_numbers)-1:
+                hundreds = number_formation(clean_numbers[thousand_index+1:])
+            elif million_index > -1 and million_index != len(clean_numbers)-1:
+                hundreds = number_formation(clean_numbers[million_index+1:])
+            elif billion_index > -1 and billion_index != len(clean_numbers)-1:
+                hundreds = number_formation(clean_numbers[billion_index+1:])
+            elif thousand_index == -1 and million_index == -1 and billion_index == -1:
+                hundreds = number_formation(clean_numbers)
+            else:
+                hundreds = 0
+            total_sum += hundreds
+
+    # adding decimal part to total_sum (if exists)
+    if len(clean_decimal_numbers) > 0:
+        decimal_sum = get_decimal_sum(clean_decimal_numbers)
+        total_sum += decimal_sum
+
+    return total_sum
+
+
+
+
+
+
 # Inicializa o reconhecedor de voz e o sintetizador de voz
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
-'''
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
@@ -30,13 +176,13 @@ def speak(text):
         engine = pyttsx3.init()
         engine.say(text)
         engine.runAndWait()
-
+'''
 def listen():
     with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source, duration=0.5)  # Ajusta para o ruído ambiente
         print("Diga algo...")
         #audio = recognizer.listen(source, timeout=3, phrase_time_limit=6)  # Limita o tempo de escuta
-        audio = recognizer.listen(source, phrase_time_limit=6)
+        audio = recognizer.listen(source, phrase_time_limit=5)
         try:
             command = recognizer.recognize_google(audio, language='pt-BR')
             print(f"Você disse: {command}")
@@ -77,10 +223,7 @@ while True:
         driver.quit()
         speak("Fechando o navegador")
         #break
-    elif "ENCERRAR" in command:
-        driver.quit()
-        speak("Encerrando o assistente")
-        break
+    
     elif "PESQUISAR" in command:
         texto_pesquisa = (command.split("PESQUISAR",1)[1])
         speak("Pesquisando" + texto_pesquisa)
@@ -90,7 +233,29 @@ while True:
         #search_box.send_keys(texto_pesquisa + Keys.RETURN)
         search_box.send_keys(texto_pesquisa)
         search_box.send_keys(Keys.RETURN)
+        #break
+    elif "ABRIR VÍDEO" in command:
+        numero_video_extenso = (command.split("ABRIR VÍDEO",1)[1])
+        numero_video_extenso = numero_video_extenso.strip()
+        numero_video = word_to_num(numero_video_extenso)
+        speak("Pesquisando" + str(numero_video))
+        xpath_video = '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[' + str(numero_video) + ']/div[1]/div/div[1]/div/h3/a/yt-formatted-string'
+        link_video = driver.find_element("xpath", xpath_video)
+        link_video.click()
+        #break
+        '''
+        #search_box = driver.find_element("xpath", '//*[@id="search"]')
+        search_box = driver.find_element("xpath", '/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input')
+        time.sleep(1)
+        #search_box.send_keys(texto_pesquisa + Keys.RETURN)
+        search_box.send_keys(texto_pesquisa)
+        search_box.send_keys(Keys.RETURN)
+        '''
 
+    elif "ENCERRAR" in command:
+        driver.quit()
+        speak("Encerrando o assistente")
+        break
     elif 'EXCEL' in command:
         speak("Abrindo o Excel")
         #subprocess.run(["start", "Excel"], shell=True)
