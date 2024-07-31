6c686232a046a331ade13c598862393464f9ab38
LucasAssis00 <92377740+LucasAssis00@users.noreply.github.com>
2024-07-31 16:49:25 -0300

    Add files via upload

--- speech-justino.py
+++ speech-justino.py
@@ -195,69 +195,146 @@ def listen():
             return ""
 
 while True:
-    command = listen().upper()
-    print(command)
-    if "NAVEGADOR" in command:
-        speak("Abrindo o navegador")
-        #subprocess.run(["start","chromium"], shell=True)
-        driver = webdriver.Edge()
-        #subprocess.run(["chromium"], shell=True)
-    elif "NOVA ABA" in command:
-        speak("nova aba")
-        driver.switch_to.new_window('tab')
-    elif "NOVA JANELA" in command:
-        speak("nova janela")
-        driver.switch_to.new_window('window')
-        #driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
-    elif "YOUTUBE" in command:
-        speak("youtube")
-        driver.get("http://www.youtube.com")
-        #driver.find_element_by_tag_name('body').send_keys('youtube.com')
-        #driver.find_element_by_tag_name('body').send_keys(Keys.ENTER)
-    elif "INSTAGRAM" in command:
-        speak("abrindo insta")
-        driver.get("https://www.instagram.com/")
-        #driver.find_element_by_tag_name('body').send_keys('instagram.com')
-        #driver.find_element_by_tag_name('body').send_keys(Keys.ENTER)
-    elif "SAIR" in command:
-        driver.quit()
-        speak("Fechando o navegador")
-        #break
-    
-    elif "PESQUISAR" in command:
-        texto_pesquisa = (command.split("PESQUISAR",1)[1])
-        speak("Pesquisando" + texto_pesquisa)
-        #search_box = driver.find_element("xpath", '//*[@id="search"]')
-        search_box = driver.find_element("xpath", '/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input')
-        time.sleep(1)
-        #search_box.send_keys(texto_pesquisa + Keys.RETURN)
-        search_box.send_keys(texto_pesquisa)
-        search_box.send_keys(Keys.RETURN)
-        #break
-    elif "ABRIR VÍDEO" in command:
-        numero_video_extenso = (command.split("ABRIR VÍDEO",1)[1])
-        numero_video_extenso = numero_video_extenso.strip()
-        numero_video = word_to_num(numero_video_extenso)
-        speak("Pesquisando" + str(numero_video))
-        xpath_video = '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[' + str(numero_video) + ']/div[1]/div/div[1]/div/h3/a/yt-formatted-string'
-        link_video = driver.find_element("xpath", xpath_video)
-        link_video.click()
-        #break
-        '''
-        #search_box = driver.find_element("xpath", '//*[@id="search"]')
-        search_box = driver.find_element("xpath", '/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input')
-        time.sleep(1)
-        #search_box.send_keys(texto_pesquisa + Keys.RETURN)
-        search_box.send_keys(texto_pesquisa)
-        search_box.send_keys(Keys.RETURN)
-        '''
+    command = listen().upper()  # string
+    match command:
+        case cmd if "NAVEGADOR" in cmd:
+            print(cmd)
+            speak("Abrindo o navegador")
+            driver = webdriver.Edge()
+        case cmd if "NOVA ABA" in cmd:
+            speak("nova aba")
+            driver.switch_to.new_window('tab')
+        case cmd if "NOVA JANELA" in cmd:
+            speak("nova janela")
+            driver.switch_to.new_window('window')
+        case cmd if "GOOGLE" in cmd:
+            speak("google")
+            driver.get("http://www.google.com")
+        case cmd if "YOUTUBE" in cmd:
+            speak("youtube")
+            driver.get("http://www.youtube.com")
+        case cmd if "INSTAGRAM" in cmd:
+            speak("abrindo instagram")
+            driver.get("https://www.instagram.com/")
+        case cmd if "FACEBOOK" in cmd:
+            speak("abrindo Facebook")
+            driver.get("https://www.facebook.com/")
+        case cmd if "FORMULÁRIO TESTE" in cmd:
+            speak("abrindo practice-automation")
+            driver.get("https://practice-automation.com/form-fields/")
+        case cmd if "CRIAR CONTA" in cmd:
+            if 'youtube' in driver.current_url:
+                xpath_register1 = '/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[3]/div[2]/ytd-button-renderer/yt-button-shape/a/yt-touch-feedback-shape/div/div[2]'
+                botao_register1 = driver.find_element("xpath", xpath_register1)
+                botao_register1.click()
+                xpath_register2 = '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[2]/div/div/div[1]/div/button/span'
+                botao_register2 = driver.find_element("xpath", xpath_register2)
+                botao_register2.click()
+                xpath_register3 = '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[2]/div/div/div[2]/div/ul/li[1]'
+                botao_register3 = driver.find_element("xpath", xpath_register3)
+                botao_register3.click()
+            if 'facebook' in driver.current_url:
+                xpath_register = '/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[5]/a'
+                botao_register = driver.find_element("xpath", xpath_register)
+                botao_register.click()
+        case cmd if "NOME" in cmd:
+            nome_user = (command.split("NOME", 1)[1]).strip()
+            search_box = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[1]/input')
+            search_box.send_keys(nome_user)
+        case cmd if "SENHA" in cmd:
+            senha_user = (command.split("SENHA", 1)[1]).strip()
+            search_box = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[2]/input')
+            search_box.send_keys(senha_user)
+        case cmd if "BEBIDA FAVORITA" in cmd:
+            if 'ÁGUA' in cmd:
+                botao_bebida = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[4]")
+                botao_bebida.click()
+            if 'LEITE' in cmd:
+                botao_bebida = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[5]")
+                botao_bebida.click()
+            if 'CAFÉ' in cmd:
+                botao_bebida = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[6]")
+                botao_bebida.click()
+            if 'VINHO' in cmd:
+                botao_bebida = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[7]")
+                botao_bebida.click()
+            if 'CHÁ' in cmd:
+                botao_bebida = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[8]")
+                botao_bebida.click()
+        case cmd if "COR FAVORITA" in cmd:
+            if 'VERMELHO' in cmd:
+                botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/input[6]")
+                #botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[10]")
+                botao_cor.click()
+            if 'AZUL' in cmd:
+                botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/input[7]")
+                #botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[11]")
+                botao_cor.click()
+            if 'AMARELO' in cmd:
+                botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/input[8]")
+                #botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[12]")
+                botao_cor.click()
+            if 'VERDE' in cmd:
+                botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/input[9]")
+                #botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[13]")
+                botao_cor.click()
+            if 'ROSA' in cmd:
+                botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/input[10]")
+                #botao_cor = driver.find_element("xpath", "/html/body/div[1]/div[2]/div/div/main/div/article/div/form/label[14]")
+                botao_cor.click()
+        case cmd if "GOSTO" in cmd:
+            botao_caixa = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/select')
+            botao_caixa.click()
+            if "NÃO SEI" in cmd:
+                botao_caixa = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/select/option[4]')
+            elif "NÃO GOSTO" in cmd:
+                botao_caixa = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/select/option[3]')
+            else:
+                botao_caixa = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/select/option[2]')
+            botao_caixa.click()
+        case cmd if "E-MAIL" in cmd:
+            #cmd.replace("ARROBA", "@")
+            email_user = (command.split("E-MAIL", 1)[1]).strip()
+            email_user.replace("ARROBA", "@")
+            email_box = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/input[11]')
+            email_box.send_keys(email_user)
+            #email_box.send_keys(Keys.RETURN)
+        case cmd if "MENSAGEM" in cmd:
+            input_message = (command.split("MENSAGEM", 1)[1]).strip()
+            message_box = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/textarea')
+            message_box.send_keys(input_message)
+        case cmd if "ENVIAR" in cmd:
+            botao_avanca = driver.find_element("xpath", '/html/body/div[1]/div[2]/div/div/main/div/article/div/form/button')
+            botao_avanca.click()
+        
 
-    elif "ENCERRAR" in command:
-        driver.quit()
-        speak("Encerrando o assistente")
-        break
-    elif 'EXCEL' in command:
-        speak("Abrindo o Excel")
-        #subprocess.run(["start", "Excel"], shell=True)
-        #subprocess.run(["Excel"], shell=True)
-
+        case cmd if "SAIR" in cmd:
+            driver.quit()
+            speak("Fechando o navegador")
+        case cmd if "PESQUISAR" in cmd:
+            texto_pesquisa = (command.split("PESQUISAR", 1)[1])
+            speak("Pesquisando" + texto_pesquisa)
+            if 'youtube' in driver.current_url:
+                search_box = driver.find_element("xpath", '/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input')
+            if 'google' in driver.current_url:
+                search_box = driver.find_element("xpath", '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea')
+            time.sleep(1)
+            search_box.send_keys(texto_pesquisa)
+            search_box.send_keys(Keys.RETURN)
+        case cmd if "ABRIR VÍDEO" in cmd:
+            numero_video_extenso = (command.split("ABRIR VÍDEO", 1)[1])
+            numero_video_extenso = numero_video_extenso.strip()
+            numero_video = word_to_num(numero_video_extenso)
+            speak("Pesquisando" + str(numero_video))
+            xpath_video = f'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{numero_video}]/div[1]/div/div[1]/div/h3/a/yt-formatted-string'
+            link_video = driver.find_element("xpath", xpath_video)
+            link_video.click()
+        case cmd if "ENCERRAR" in cmd:
+            try:
+                driver.quit()
+            except:
+                pass
+            speak("Encerrando o assistente")
+            break
+        case cmd if "EXCEL" in cmd:
+            speak("Abrindo o Excel")
