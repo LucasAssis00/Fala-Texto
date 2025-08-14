9ff3127c2eab622296cb470c06e89a897dd43742
LucasAssis00 <92377740+LucasAssis00@users.noreply.github.com>
2025-08-14 16:52:41 -0300

    Update build.gradle.kts

--- App-kotlin/CirurgiaSegura/build.gradle.kts
+++ App-kotlin/CirurgiaSegura/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.uploadcirurgiasegura"
+    namespace = "com.example.uploadcirurgiasegura2"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.uploadcirurgiasegura"
+        applicationId = "com.example.uploadcirurgiasegura2"
         minSdk = 30
         targetSdk = 35
         versionCode = 1
